1226c744c8db84abeefafae32217b806f4eee3ee
nadu <nadu@WINDOWS-UINETAB>
2024-05-06 03:51:37 -0700

    Add project files.

---
+++ labsheet 02 question 2/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3_Q2
+{
+    internal class BankAccount
+    {
+        public string Account number
+        public decimal Balance
+
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount();
+
+            Console.Write("Enter the account number: ");
+            account.AccountNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the deposit amount:");
+            double amount = double.Parse(Console.ReadLine());
+
+            account.deposit(amount);
+
+            Console.WriteLine("Account number:" + account.AccountNumber);
+            Console.WriteLine()
+        }
+    }
+}
